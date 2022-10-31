817bc77de461d3cf55649b6694cab97eb7f8b100
Bailey Cage <bailey.cage@gmail.com>
2022-10-31 11:38:25 -0400

    add uniqueness verifier

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,4 +1,8 @@
 class Traveler < ApplicationRecord
+   validates_presence_of :user_id, :trip_id
+   validates :user_id, {
+      uniqueness: {scope: :trip_id, message: 'cannot duplicate entry'}
+   }
    belongs_to :user
    belongs_to :trip
 end
