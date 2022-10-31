c4049bae797e1b2029829da1ecca12693533416b
Bailey Cage <bailey.cage@gmail.com>
2022-10-31 12:48:05 -0400

    move to cards folder

--- client/src/components/cards/TripCard.jsx
+++ client/src/components/cards/TripCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function TripCard (props) {
    const { trip } = props
-   const { name, num_days } = trip
+   const { id, name, num_days } = trip
+   const navigate = useNavigate()
    return (
-      <div className='card'>
+      <div className='card' onClick={() => navigate(`${id}`, {state: {trip: trip}})}>
          <div>{name}</div>
          <p>{num_days} day{num_days > 1 ? 's' : null}</p>
       </div>
